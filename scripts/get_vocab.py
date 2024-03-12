2055988c22418db9a0f0a086627ee94883a9a380
yingzibu <46125227+yingzibu@users.noreply.github.com>
2024-03-12 19:15:28 -0400

    Update get_vocab.py

--- scripts/get_vocab.py
+++ scripts/get_vocab.py
@@ -101,22 +101,7 @@ def get_vocab(train: pd.DataFrame, vocab_type=VOCAB_TYPE):
     print('alphabet len: ', len(alphabet), ' add helper token: ', len(all_sys))
     return all_sys 
 
-# get_vocab(trains, 'selfies')
 
-# def get_vocab(train: pd.DataFrame, vocab_type=VOCAB_TYPE): 
-#     df = train.copy()
-#     for col_smi in ['smiles', 'Drug', 'SMILES', 'Smiles', 'smile']: 
-#         if col_smi in df.columns: smiles = list(df[col_smi]); break
-#     assert isinstance(smiles, list) == True
-#     assert len(df) == len(smiles)
-#     chars = set()
-#     for string in smiles: 
-#         if vocab_type == 'char': chars.update(string) # create an alphabet set
-#         elif vocab_type == 'smiles': chars.update(smiles_tokenizer(string))
-        
-#     alphabet = sorted(list(chars))
-#     all_sys =  ['<pad>', '<bos>', '<eos>', '<unk>'] + alphabet
-#     return all_sys 
 
 def get_c2i_i2c(all_sys): # input alphabet list
     c2i = {c: i for i, c in enumerate(all_sys)}
@@ -131,14 +116,6 @@ def id2char(id, i2c, c2i):
     if id not in i2c: return i2c[c2i['<unk>']]
     else: return i2c[id]
 
-# def string2ids(string, c2i, add_bos=False, add_eos=False, vocab_type=VOCAB_TYPE):
-#     if vocab_type == 'char': ids = [char2id(c, c2i) for c in string]
-#     elif vocab_type == 'smiles': 
-#         tokens = smiles_tokenizer(string) 
-#         ids = [char2id(t, c2i) for t in tokens]
-#     if add_bos: ids = [c2i['<bos>']] + ids
-#     if add_eos: ids = ids + [c2i['<eos>']]
-#     return ids
 def string2ids(string, c2i, add_bos=False, add_eos=False, vocab_type=VOCAB_TYPE):
     if vocab_type == 'char': ids = [char2id(c, c2i) for c in string]
     elif vocab_type == 'smiles': 
@@ -176,62 +153,4 @@ def string2tensor(string, c2i, vocab_type=VOCAB_TYPE, device='cpu'):
     tensor = torch.tensor(ids, dtype=torch.long, device=device)
     return tensor
 
-# def get_vocab(train: pd.DataFrame, vocab_type='char'): 
-#     df = train.copy()
-#     for col_smi in ['smiles', 'Drug', 'SMILES', 'Smiles', 'smile']: 
-#         if col_smi in df.columns: smiles = list(df[col_smi]); break
-#     if vocab_type == 'char': 
-#         chars = set()
-#         for string in smiles: chars.update(string) # create an alphabet set
-#         all_sys =  ['<pad>', '<bos>', '<eos>', '<unk>'] + sorted(list(chars))
-#     return all_sys 
-
-# def get_c2i_i2c(all_sys): # input alphabet list
-#     c2i = {c: i for i, c in enumerate(all_sys)}
-#     i2c = {i: c for i, c in enumerate(all_sys)}
-#     return c2i, i2c
-
-# def char2id(char, c2i):
-#     if char not in c2i: return c2i['<unk>']
-#     else: return c2i[char]
-
-# def id2char(id, i2c, c2i):
-#     if id not in i2c: return i2c[c2i['<unk>']]
-#     else: return i2c[id]
-
-# def string2ids(string, c2i, add_bos=False, add_eos=False):
-#     ids = [char2id(c, c2i) for c in string]
-#     if add_bos: ids = [c2i['<bos>']] + ids
-#     if add_eos: ids = ids + [c2i['<eos>']]
-#     return ids
-
-# def ids2string(ids, c2i, i2c, rem_bos=True, rem_eos=True):
-#     # print(ids)
-#     if isinstance(ids[0], list): ids = ids[0]
-#     if len(ids) == 0: return ''
-#     if rem_bos and ids[0] == c2i['<bos>']: ids = ids[1:]
-#     # delete <eos>
-#     if rem_eos:
-#         for i, id in enumerate(ids):
-#             if id == c2i['<eos>']: ids = ids[:i]; break
-#     string = ''.join([id2char(id, i2c, c2i) for id in ids])
-#     return string
-
-# def string2tensor(string, c2i, device='cuda'):
-#     # c2i, i2c = get_c2i_i2c(vocab)
-#     ids = string2ids(string, c2i, add_bos=True, add_eos=True)
-#     tensor = torch.tensor(ids, dtype=torch.long, device=device)
-#     return tensor
-
-
-# vocab = get_vocab(train)
-# c2i, i2c = get_c2i_i2c(vocab)
-# c2i, i2c
-# char2id('(', c2i)
-
-# id2char(5, i2c, c2i)
-
-# string2ids('(fa', c2i)
-# ids2string([9, 4, 5], c2i, i2c)
-# string = 'CCO'
-# string2tensor(string, vocab, device='cuda')
+ 
